dacfe1abf501af4255c9a5824ee4bcaf120584cc
EagleCharles <sirnwa21@gmail.com>
2023-10-30 16:57:34 +0100

    my first javascript practice

---
+++ first- assignment/index.js
@@ -0,0 +1,35 @@
+console.log("Hello World!");
+
+let a = 823;
+let b = 7482;
+let c = 54156;
+let d = 516;
+let e = 546;
+let f = 98;
+let g = 7491;
+let h = 3541;
+let i = 8423;
+let j = 1561;
+
+let add = a + b + c + d + e + f + g + h + i + j;
+
+const avg = add / 10;
+
+console.log(avg);
+
+a = (823 - avg) ** 2;
+b = (7482 - avg) ** 2;
+c = (54156 - avg) ** 2;
+d = (516 - avg) ** 2;
+e = (546 - avg) ** 2;
+f = (98 - avg) ** 2;
+g = (7491 - avg) ** 2;
+h = (3541 - avg) ** 2;
+i = (8423 - avg) ** 2;
+j = (1561 - avg) ** 2;
+
+add = a + b + c + d + e + f + g + h + i + j;
+
+const variance = add / 10;
+
+console.log(variance);
